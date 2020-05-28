e53d67af3b0e712888d5256caeae4a657ffba8c7
SvetLanaDukk <dukkardtsv@gmail.com>
2020-05-28 15:19:06 -0400

    header

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+    
+
+    $('.product-slider__inner').slick({
+
+        dots: true
+    })
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        readOnly:true,
+        starWidth: "12px"
+      });
+
+var mixer = mixitup ('.products__inner-box');
+});
