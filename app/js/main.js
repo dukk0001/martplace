806ad6b54ad7e72b4cddc8e6c535e9b3fc59fe51
SvetLanaDukk <dukkardtsv@gmail.com>
2020-06-08 00:07:54 -0400

    without mixitup

--- app/js/main.js
+++ app/js/main.js
@@ -8,9 +8,32 @@ $(function(){
 
     $(".rate-star").rateYo({
         rating: 5,
-        readOnly:true,
-        starWidth: "12px"
+        readOnly:false,
+        starWidth: "15px"
       });
+
+
+    
+ $('.slider__inner-display').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: false,
+    asNavFor: '.slider__inner-arrows'
+  });
+  $('.slider__inner-arrows').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.slider__inner-display',
+    arrows: true,
+    dots:false,
+    centerMode: true,
+    focusOnSelect: true
+  });
+
+
+
+ 
 
 var mixer = mixitup ('.products__inner-box');
 });
