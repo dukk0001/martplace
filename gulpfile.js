5700127a5951fee68eb969404b14a1fbcdc9e056
Svetlana Dukkardt <dukkardtsv@gmail.com>
2020-08-16 15:04:56 -0400

    added missing file

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,51 @@
- let gulp= require('gulp'); 
- let sass=require('gulp-sass');
- let rename=require('gulp-rename'); 
- let browserSync=require('browser-sync');
- let autoprefixer=require('gulp-autoprefixer');
- let concat=require ('gulp-concat');
- let uglify=require('gulp-uglify');
- let cssmin=require('gulp-cssmin');
+ let gulp = require('gulp');
+ let sass = require('gulp-sass');
+ let rename = require('gulp-rename');
+ let browserSync = require('browser-sync');
+ let autoprefixer = require('gulp-autoprefixer');
+ let concat = require('gulp-concat');
+ let uglify = require('gulp-uglify');
+ let cssmin = require('gulp-cssmin');
 
 
- gulp.task('sass', function(){
-    return gulp.src('app/scss/**/*.scss')
-     .pipe(sass({outputStyle: 'expanded'}))
-     .pipe(rename({suffix:'.min'}))
-     .pipe(autoprefixer({
-        overrideBrowserslist:['last 7 versions']
-     }))
-     .pipe(gulp.dest('app/css'))
-     .pipe(browserSync.reload({stream: true}))
+ gulp.task('sass', function() {
+     return gulp.src('app/scss/**/*.scss')
+         .pipe(sass({ outputStyle: 'expanded' }))
+         .pipe(rename({ suffix: '.min' }))
+         .pipe(autoprefixer({
+             overrideBrowserslist: ['last 7 versions']
+         }))
+         .pipe(gulp.dest('app/css'))
+         .pipe(browserSync.reload({ stream: true }))
  });
 
- gulp.task('style', function(){
-    return gulp.src([
-        'node_modules/normalize.css/normalize.css',
-        'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css',
-        'node_modules/rateyo/src/jquery.rateyo.css',
-        'app/css/justselect.min.css'
-    ])
-    .pipe(concat('libs.min.css'))
-    .pipe(cssmin())
-    .pipe(gulp.dest('app/css'))
-})
-
- gulp.task('script', function(){
+ gulp.task('style', function() {
      return gulp.src([
-         'node_modules/slick-carousel/slick/slick.js',
-         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-         'node_modules/mixitup/dist/mixitup.js',
-         'node_modules/rateyo/src/jquery.rateyo.js',
-         'app/js/justselect.min.js'
-     ])
-     .pipe(concat('libs.min.js'))
-     .pipe(uglify())
-     .pipe(gulp.dest('app/js'))
+             'node_modules/normalize.css/normalize.css',
+             'node_modules/slick-carousel/slick/slick.css',
+             'node_modules/magnific-popup/dist/magnific-popup.css',
+             'node_modules/rateyo/src/jquery.rateyo.css',
+             'app/scss/justselect.min.css'
+         ])
+         .pipe(concat('libs.min.css'))
+         .pipe(cssmin())
+         .pipe(gulp.dest('app/css'))
  })
 
- gulp.task('watch', function(){
+ gulp.task('script', function() {
+     return gulp.src([
+             'node_modules/slick-carousel/slick/slick.js',
+             'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+             'node_modules/mixitup/dist/mixitup.js',
+             'node_modules/rateyo/src/jquery.rateyo.js',
+             'app/js/justselect.min.js'
+         ])
+         .pipe(concat('libs.min.js'))
+         .pipe(uglify())
+         .pipe(gulp.dest('app/js'))
+ })
+
+ gulp.task('watch', function() {
      gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
      gulp.watch('app/*.html', gulp.parallel('html'));
      gulp.watch('app/js/*.js', gulp.parallel('js'));
@@ -53,21 +53,21 @@
 
  });
 
- gulp.task('html',function(){
+ gulp.task('html', function() {
      return gulp.src('app/*.html')
-     .pipe(browserSync.reload({stream:true}))
+         .pipe(browserSync.reload({ stream: true }))
  });
- gulp.task('js',function(){
-    return gulp.src('app/js/*.js')
-    .pipe(browserSync.reload({stream:true}))
-});
+ gulp.task('js', function() {
+     return gulp.src('app/js/*.js')
+         .pipe(browserSync.reload({ stream: true }))
+ });
 
  gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "app/"
-        }
-    });
-});
+     browserSync.init({
+         server: {
+             baseDir: "app/"
+         }
+     });
+ });
 
-gulp.task('default', gulp.parallel('style','script','sass','watch','browser-sync'));
+ gulp.task('default', gulp.parallel('style', 'script', 'sass', 'watch', 'browser-sync'));
